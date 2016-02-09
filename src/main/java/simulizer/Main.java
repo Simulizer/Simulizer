8209821a4643d0d2ceaf5fb54ba43a243ac3a081
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-02-09 23:46:43 +0000

    Removed all traces of jar extraction code

--- src/main/java/simulizer/Main.java
+++ src/main/java/simulizer/Main.java
@@ -27,44 +27,8 @@ public class Main extends Application {
 		// Set icon as logo.png
 		primaryStage.getIcons().add(new Image(getResource("logo.png").toString()));
 
-		// Copy folders from resources to jar running directory
-		//copyResource("code");
-		//copyResource("layouts");
-		//copyResource("themes");
-
 		// Just show the main window for now
 		new WindowManager(primaryStage);
-	}
-
-	// Thanks to: http://stackoverflow.com/questions/11012819/how-can-i-get-a-resource-folder-from-inside-my-jar-file#answer-20073154
-	private void copyResource(String relFile) throws IOException, URISyntaxException {
-		Path outsideFolder = Paths.get(System.getProperty("user.dir") + File.separator + relFile);
-		if (!Files.exists(outsideFolder)) {
-			final File jarFile = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
-			if (jarFile.isFile()) {
-				// Running in a JAR file
-				final JarFile jar = new JarFile(jarFile);
-				final Enumeration<JarEntry> entries = jar.entries(); // gives ALL entries in jar
-				while (entries.hasMoreElements()) {
-					final JarEntry entry = entries.nextElement();
-					// filter according to the path
-					if (entry.getName().startsWith(relFile + "/")) { 
-						// TODO: Copy files from within the JAR
-						System.out.println(entry.getName());
-					}
-				}
-				jar.close();
-			} else {
-				// Running in an IDE
-				Path insideFolder = Paths.get(getResource(relFile));
-				if (Files.isDirectory(insideFolder)) {
-					Files.createDirectories(outsideFolder);
-					Files.walkFileTree(insideFolder, new CopyFileVisitor(outsideFolder));
-				} else {
-					Files.copy(insideFolder, outsideFolder);
-				}
-			}
-		}
 	}
 
 	private URI getResource(String filepath) throws URISyntaxException {
