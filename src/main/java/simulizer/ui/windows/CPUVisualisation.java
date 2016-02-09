40eaba6148f59a7450406c39cd9c57b4a2b83869
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-02-09 23:42:05 +0000

    Fixed git's mistake, project should compile now.

--- src/main/java/simulizer/ui/windows/CPUVisualisation.java
+++ src/main/java/simulizer/ui/windows/CPUVisualisation.java
@@ -4,48 +4,47 @@ import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import simulizer.cpu.visualisation.CPU;
 import simulizer.ui.interfaces.InternalWindow;
+import simulizer.ui.theme.Theme;
 
 public class CPUVisualisation extends InternalWindow {
 
-    int width;
-    int height;
-    Pane pane;
+	int width;
+	int height;
+	Pane pane;
 
 	public CPUVisualisation() {
-        width = 600;
-        height = 350;
-        pane = new Pane();
-        pane.setPrefHeight(height);
-        pane.setPrefWidth(width);
-        pane.setMinHeight(height);
-        pane.setMinWidth(width);
-        pane.setMaxHeight(height);
-        pane.setMaxWidth(width);
-        getChildren().add(pane);
+		width = 600;
+		height = 350;
+		pane = new Pane();
+		pane.setPrefHeight(height);
+		pane.setPrefWidth(width);
+		pane.setMinHeight(height);
+		pane.setMinWidth(width);
+		pane.setMaxHeight(height);
+		pane.setMaxWidth(width);
+		getChildren().add(pane);
 		drawVisualisation();
 	}
 
-    @Override
-    public void setTheme(String theme) {
-        super.setTheme(theme);
-        getStylesheets().clear();
-        getStylesheets().add(theme + "/window.css");
-        getStylesheets().add(theme + "/cpu.css");
-    }
+	@Override
+	public void setTheme(Theme theme) {
+		super.setTheme(theme);
+		getStylesheets().add(theme.getStyleSheet("cpu.css"));
+	}
 
-    public void add(Node e){
-        pane.getChildren().add(e);
-    }
+	public void add(Node e) {
+		pane.getChildren().add(e);
+	}
 
-    public void addAll(Node... elements){
-        pane.getChildren().addAll(elements);
-    }
+	public void addAll(Node... elements) {
+		pane.getChildren().addAll(elements);
+	}
 
 	private void drawVisualisation() {
-        CPU cpu = new CPU(this, width, height);
-        cpu.drawCPU();
+		CPU cpu = new CPU(this, width, height);
+		cpu.drawCPU();
 	}
-	
+
 	@Override
 	protected double getMinimalHeight() {
 		return 380;
