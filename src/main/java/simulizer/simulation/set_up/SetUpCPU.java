247590c8fc777ae9608a321c98abb9de06b0ac42
charlie1329 <charlie.street1@btinternet.com>
2016-02-10 17:25:07 +0000

    finished set up I think

--- src/main/java/simulizer/simulation/set_up/SetUpCPU.java
+++ src/main/java/simulizer/simulation/set_up/SetUpCPU.java
@@ -1,6 +1,13 @@
 package simulizer.simulation.set_up;
 
+import java.math.BigInteger;
+import java.util.Map;
+
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
+
 import simulizer.simulation.cpu.components.*;
+import simulizer.simulation.data.representation.Word;
 
 /**this class will take a program object received from the assembler
  * and use this to set up our cpu
@@ -24,12 +31,16 @@ public class SetUpCPU
 	 */
 	public SetUpCPU(Program programData)
 	{
-		this.mainMemory = new MainMemory(null,null);//fill in
+		this.programData = programData;//all program information
+		
+		this.mainMemory = new MainMemory(this.programData.textSegment,this.programData.dataSegment,this.programData.textSegmentStart,
+				this.programData.dataSegmentStart, this.programData.dynamicSegmentStart);
+		
 		this.registers = new RegisterBlock();
 		this.controlUnit = new ControlUnit(null,null,this.registers,null,null);//setting null to start with to make the linking easier
 		this.ALU = new ALU(this.registers,this.controlUnit);
 		this.LSUnit = new LSUnit(this.registers,this.mainMemory,null,this.controlUnit);
-		this.programCounter = new ProgramCounter(null,this.controlUnit,null,this.LSUnit);
+		this.programCounter = new ProgramCounter(this.getFirstInstruction(),this.controlUnit,null,this.LSUnit);
 		this.instructionRegister = new InstructionRegister(this.controlUnit,this.programCounter,this.LSUnit);
 		//initialise memory, do all set calls and sort oout program
 		
@@ -41,7 +52,26 @@ public class SetUpCPU
 		this.LSUnit.setInstructionRegister(this.instructionRegister);
 		this.programCounter.setInstructionRegister(this.instructionRegister);
 		
+	
+	}
+	
+	/**method will look through the program object to get the address associated with the maLabelbel
+	 * 
+	 */
+	private Word getFirstInstruction()
+	{
+		Map<Label,Address> labels = this.programData.labels;
 		
+		for(Map.Entry<Label, Address> entry : labels.entrySet())//iterate through map
+		{
+			if(entry.getKey().getName().toLowerCase().equals("main"))//if main found
+			{
+				BigInteger address = BigInteger.valueOf(entry.getValue().getValue());//put itinto word format
+				return new Word(address);
+			}
+		}
+		
+		return null;//if main not in (would be a bad program if this was the case)
 	}
 	
 }
