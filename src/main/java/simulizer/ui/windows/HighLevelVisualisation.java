39e0f71dd4c609b2d9def34fd703c93a916cd97e
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-02-17 15:11:20 +0000

    High-Level now draws Tower Of Hanoi Visualiser

--- src/main/java/simulizer/ui/windows/HighLevelVisualisation.java
+++ src/main/java/simulizer/ui/windows/HighLevelVisualisation.java
@@ -2,34 +2,38 @@ package simulizer.ui.windows;
 
 import javafx.scene.layout.Pane;
 import simulizer.highlevel.visualisation.DataStructureVisualiser;
+import simulizer.highlevel.visualisation.TowerOfHanoiVisualiser;
 import simulizer.ui.interfaces.InternalWindow;
 import simulizer.ui.theme.Theme;
 
 public class HighLevelVisualisation extends InternalWindow {
 	private DataStructureVisualiser visualiser;
 	private Pane drawingPane;
-	
-	public HighLevelVisualisation() {
-		init();
-	}
-	
+
 	private void init() {
 		this.drawingPane = new Pane();
+		visualiser = new TowerOfHanoiVisualiser(drawingPane, (int) getWidth(), (int) getHeight(), 0, 5);
 		getChildren().add(drawingPane);
 	}
-	
+
 	public void setVisualiser(DataStructureVisualiser visualiser) {
 		this.visualiser = visualiser;
 	}
-	
+
 	public DataStructureVisualiser getVisualiser() {
 		return this.visualiser;
 	}
-	
+
 	public Pane getDrawingPane() {
 		return drawingPane;
 	}
-	
+
+	@Override
+	public void ready() {
+		init();
+		super.ready();
+	}
+
 	@Override
 	public void setTheme(Theme theme) {
 		super.setTheme(theme);
