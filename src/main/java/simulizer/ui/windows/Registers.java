e6d2955fbc42fe4530e088d65f662b096a6e0427
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-02-17 18:04:41 +0000

    Registers can manually be viewed. Does require manual update

--- src/main/java/simulizer/ui/windows/Registers.java
+++ src/main/java/simulizer/ui/windows/Registers.java
@@ -1,51 +1,64 @@
 package simulizer.ui.windows;
 
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import simulizer.assembler.representation.Register;
+import simulizer.simulation.data.representation.Word;
 import simulizer.ui.interfaces.InternalWindow;
 
 public class Registers extends InternalWindow {
 	private TableView<Data> table = new TableView<Data>();
 
-	@SuppressWarnings("unchecked")
-	public Registers() {
-		TableColumn<Data, String> register = new TableColumn<Data, String>("Register");
-		register.setCellValueFactory(new PropertyValueFactory<Data, String>("name"));
-		TableColumn<Data, String> value = new TableColumn<Data, String>("Value");
-		value.setCellValueFactory(new PropertyValueFactory<Data, String>("value"));
-		init();
-		table.getColumns().addAll(register, value);
-		table.setEditable(false);
-		getContentPane().getChildren().add(table);
-	}
-
-	private void init() {
+	public void refreshData() {
 		ObservableList<Data> data = FXCollections.observableArrayList();
 		for (Register r : Register.values()) {
-			data.add(new Data(r.getName(), "00000000"));
+			String value = "";
+			try {
+				Word[] registers = getWindowManager().getRegisters();
+				for (byte b : registers[0].getWord()) {
+					value += b;
+				}
+			} catch (NullPointerException e) {
+				value = "EMPTY";
+			}
+			data.add(new Data(r.getName(), value));
 		}
 		table.setItems(data);
 	}
 
+	@Override
+	@SuppressWarnings("unchecked")
+	public void ready() {
+		TableColumn<Data, String> register = new TableColumn<Data, String>("Register");
+		register.setCellValueFactory(new PropertyValueFactory<Data, String>("name"));
+		TableColumn<Data, String> value = new TableColumn<Data, String>("Value");
+		value.setCellValueFactory(new PropertyValueFactory<Data, String>("value"));
+		refreshData();
+		table.getColumns().addAll(register, value);
+		table.setEditable(false);
+		getContentPane().getChildren().add(table);
+		super.ready();
+	}
+
 	public static class Data {
-		private final SimpleStringProperty name;
-		private final SimpleStringProperty value;
+
+		private String name;
+		private String value;
 
 		public Data(String name, String value) {
-			this.name = new SimpleStringProperty(name);
-			this.value = new SimpleStringProperty(value);
+			this.name = name;
+			this.value = value;
 		}
+
 		public String getName() {
-			return name.get();
+			return name;
 		}
 
 		public String getValue() {
-			return value.get();
+			return value;
 		}
 
 	}
