f2139c6d0f76b662053e47cfd3f0627939ec8e09
Kelsey McKenna <kelseyguitar55@googlemail.com>
2016-02-26 16:03:29 +0000

    Added echos

--- copytosvn.sh
+++ copytosvn.sh
@@ -1,8 +1,12 @@
 SVNREPO=../A4
 
+echo "Copying files to ${SVNREPO} ..."
 for f in $(git ls-tree -r master --name-only); do
   cp --parents ${f} ${SVNREPO}
 done
-
+ 
+echo "Writing git log ..."
 git log > ${SVNREPO}/git-log.txt
+
+echo "Generating statistics document ..."
 gitinspector --format=html --timeline --weeks --responsibilities >  ${SVNREPO}/stats.html
