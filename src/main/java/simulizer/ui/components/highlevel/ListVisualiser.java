219325c0aa846b36c339c440c9b728aa3ed9b524
Kelsey McKenna <kelseyguitar55@googlemail.com>
2016-03-16 22:12:23 +0000

    List swapping is animated

--- src/main/java/simulizer/ui/components/highlevel/ListVisualiser.java
+++ src/main/java/simulizer/ui/components/highlevel/ListVisualiser.java
@@ -1,12 +1,12 @@
 package simulizer.ui.components.highlevel;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Observable;
 import java.util.Queue;
 
 import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -29,16 +29,25 @@ public class ListVisualiser extends DataStructureVisualiser {
 
 	private final Queue<Action> actionQueue = new LinkedList<>();
 	private boolean animating = false;
-	private int animatedItemIndex;
-	private DoubleProperty animatedItemX = new SimpleDoubleProperty();
-	private DoubleProperty animatedItemY = new SimpleDoubleProperty();
-	private int FRAME_RATE = 45;
+
+	private int animatedLeftIndex;
+	private String animatedLeftLabel;
+	// Ratios of the width and height
+	private DoubleProperty animatedLeftX = new SimpleDoubleProperty();
+	private DoubleProperty animatedLeftY = new SimpleDoubleProperty();
+
+	private int animatedRightIndex;
+	private String animatedRightLabel;
+	// Ratios of the width and height
+	private DoubleProperty animatedRightX = new SimpleDoubleProperty();
+	private DoubleProperty animatedRightY = new SimpleDoubleProperty();
+
+	private int FRAME_RATE = 10;
 	private AnimationTimer timer = new AnimationTimer() {
 		long lastTime = -1;
 
 		@Override
 		public void handle(long now) {
-			// 30 FPS
 			if (lastTime == -1 || now - lastTime > 1e9 / FRAME_RATE) {
 				lastTime = now;
 				repaint();
@@ -52,6 +61,8 @@ public class ListVisualiser extends DataStructureVisualiser {
 	private double rectLength;
 	private double y0;
 	private double x0;
+	private double w;
+	private double h;
 
 	public ListVisualiser(ListModel model, HighLevelVisualisation vis) {
 		super(model, vis);
@@ -66,6 +77,7 @@ public class ListVisualiser extends DataStructureVisualiser {
 		gc.setLineWidth(2);
 		gc.setStroke(Color.BLACK);
 		gc.setTextBaseline(VPos.CENTER);
+		gc.setTextAlign(TextAlignment.CENTER);
 
 		repaint();
 	}
@@ -74,34 +86,75 @@ public class ListVisualiser extends DataStructureVisualiser {
 	public void update(Observable o, Object obj) {
 		super.update(o, obj);
 		actionQueue.add((Action) obj);
+		this.list = model.getList();
 		runAnimations();
 	}
 
 	private void runAnimations() {
-		if (animating)
-			return;
+		if (animating) return;
 		else {
 			Action action = actionQueue.poll();
 
 			// Currently only swaps are implemented
-			if (!(action instanceof Swap))
-				return;
+			if (!(action instanceof Swap)) return;
 
 			Swap swap = (Swap) action;
 
-			Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1.3), e -> {
-				// Apply Update
-				list = swap.list;
+			animatedLeftIndex = swap.a;
+			animatedRightIndex = swap.b;
 
-				animating = false;
-				repaint();
+			// This is correct
+			animatedLeftLabel = "" + list[animatedRightIndex];
+			animatedRightLabel = "" + list[animatedLeftIndex];
 
-				if (actionQueue.isEmpty()) {
-					timer.stop();
-					System.out.println("List animation timer stopped");
-				} else
-					runAnimations();
-			}));
+			double startXLeft = getX(animatedLeftIndex) / w;
+			double startXRight = getX(animatedRightIndex) / w;
+
+			double startY = y0 / h;
+			double upY = 0.8 * (y0 - rectLength) / h;
+
+			// @formatter:off
+			Timeline timeline = new Timeline(
+				new KeyFrame(Duration.seconds(0),
+					new KeyValue(animatedLeftX, startXLeft),
+					new KeyValue(animatedLeftY, startY),
+					new KeyValue(animatedRightX, startXRight),
+					new KeyValue(animatedRightY, startY)
+				),
+				new KeyFrame(Duration.seconds(0.5),
+					new KeyValue(animatedLeftX, startXLeft),
+					new KeyValue(animatedLeftY, startY),
+					new KeyValue(animatedRightX, startXRight),
+					new KeyValue(animatedRightY, upY)
+				),
+				new KeyFrame(Duration.seconds(0.8),
+					new KeyValue(animatedLeftX, startXRight),
+					new KeyValue(animatedLeftY, startY),
+					new KeyValue(animatedRightX, startXLeft),
+					new KeyValue(animatedRightY, upY)
+				),
+				new KeyFrame(Duration.seconds(1.3),
+					e -> {
+						// Apply Update
+						list = swap.list;
+
+						animating = false;
+						repaint();
+
+						synchronized (actionQueue) {
+							if (actionQueue.isEmpty()) {
+								timer.stop();
+								System.out.println("List animation timer stopped");
+							} else runAnimations();
+						}
+					},
+					new KeyValue(animatedLeftX, startXRight),
+					new KeyValue(animatedLeftY, startY),
+					new KeyValue(animatedRightX, startXLeft),
+					new KeyValue(animatedRightY, startY)
+				)
+			);
+			// @formatter:on
 
 			timeline.setCycleCount(1);
 			timeline.setRate(actionQueue.size() + 1); // TODO: Be more accurate
@@ -115,51 +168,64 @@ public class ListVisualiser extends DataStructureVisualiser {
 
 	@Override
 	public void repaint() {
-		if (model.size() == 0)
-			return;
+		if (model.size() == 0) return;
 
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-		final double width = canvas.getWidth();
-		final double height = canvas.getHeight();
+		this.w = canvas.getWidth();
+		this.h = canvas.getHeight();
 
-		calculateDimensions(gc, width, height);
+		gc.clearRect(0, 0, w, h);
 
-		// TODO Platform.runLater() ?
-		gc.clearRect(0, 0, width, height);
+		calculateDimensions(gc);
+
 		drawList(gc);
+		if (animating) {
+			drawTextBox(gc, animatedLeftX.doubleValue() * w, animatedLeftY.doubleValue() * h, animatedLeftLabel);
+			drawTextBox(gc, animatedRightX.doubleValue() * w, animatedRightY.doubleValue() * h, animatedRightLabel);
+		}
 	}
 
 	private void drawList(GraphicsContext gc) {
 		for (int i = 0; i < list.length; ++i) {
-			drawBorderedRectangle(gc, Color.SKYBLUE, getX(i), y0, rectLength, rectLength);
-
-			gc.setTextAlign(TextAlignment.CENTER);
-			gc.setTextBaseline(VPos.CENTER);
-			gc.setTextAlign(TextAlignment.CENTER);
-			gc.setFont(new Font("Arial", 55));
-			gc.setFill(Color.BLACK);
-			gc.fillText(list[i] + "", getX(i) + rectLength / 2, y0 + rectLength / 2);
+			if (animating && (i == animatedLeftIndex || i == animatedRightIndex)) continue;
+			else drawTextBox(gc, i, list[i] + "");
 		}
+	}
+
+	private void drawTextBox(GraphicsContext gc, int i, String text) {
+		drawTextBox(gc, getX(i), y0, text);
+	}
+
+	private void drawTextBox(GraphicsContext gc, double x, double y, String text) {
+		drawBorderedRectangle(gc, Color.SKYBLUE, x, y, rectLength, rectLength);
+
+		gc.setFont(new Font("Arial", 55));
+		gc.setFill(Color.BLACK);
+		gc.beginPath();
+		gc.fillText(text, x + rectLength / 2, y + rectLength / 2, rectLength);
+		gc.closePath();
 	}
 
 	private double getX(int rectIndex) {
 		return x0 + rectIndex * rectLength;
 	}
 
-	private void calculateDimensions(GraphicsContext gc, double width, double height) {
-		double rectCalc = width - 2 * XPAD;
+	private void calculateDimensions(GraphicsContext gc) {
+		double rectCalc = w - 2 * XPAD;
 
-		rectLength = rectCalc < height - 2 * YPAD ? rectCalc : height - 2 * YPAD;
+		rectLength = rectCalc < h - 2 * YPAD ? rectCalc : h - 2 * YPAD;
 		this.rectLength = rectCalc / model.size();
 
-		this.x0 = (width - model.size() * rectLength) / 2;
-		this.y0 = height / 2 - rectLength / 2;
+		this.x0 = (w - model.size() * rectLength) / 2;
+		this.y0 = h / 2 - rectLength / 2;
 	}
 
 	private void drawBorderedRectangle(GraphicsContext gc, Color fill, double x, double y, double w, double h) {
+		gc.beginPath();
 		gc.setFill(fill);
 		gc.fillRect(x, y, w, h);
 		gc.strokeRect(x, y, w, h);
+		gc.closePath();
 	}
 
 	@Override
