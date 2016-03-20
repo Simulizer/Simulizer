46077a536eddb545cbbe33fb5f8f2a868fbd4d9f
mbway <matthewbroadway@msn.com>
2016-03-20 22:22:40 +0000

    added GUID to ended exception

--- src/main/java/simulizer/simulation/exceptions/EndedException.java
+++ src/main/java/simulizer/simulation/exceptions/EndedException.java
@@ -4,5 +4,5 @@ package simulizer.simulation.exceptions;
  * @author mbway
  */
 public class EndedException extends Exception {
-
+	private static final long serialVersionUID = 6831820596428405690L;
 }
