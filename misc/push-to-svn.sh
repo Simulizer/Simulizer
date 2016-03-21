4cd2aae58120921fd36e96038905fc8ba9d08f8a
Kelsey McKenna <kelseyguitar55@googlemail.com>
2016-03-21 23:20:18 +0000

    Copy everything to final folder & git log/diff

--- misc/push-to-svn.sh
+++ misc/push-to-svn.sh
@@ -5,16 +5,18 @@ SVNREPO=../A4
 
 # Delete everything (other than hidden files) from the svn repo
 cd ${SVNREPO}
-ls | xargs rm -rf
+rm -rf final
 cd -
 
-echo "Copying files to ${SVNREPO} ..."
+echo "Copying files to ${SVNREPO}/final ..."
 for f in $(git ls-tree -r master --name-only); do
-  cp --parents ${f} ${SVNREPO}
+  cp --parents ${f} ${SVNREPO}/final
 done
  
+echo "Writing git history ..."
+git log -p > ${SVNREPO}/final/git-history.txt
 echo "Writing git log ..."
-git log -p > ${SVNREPO}/git-log.txt
+git log > ${SVNREPO}/final/git-log.txt
 
 # echo "Generating statistics document ..."
 # gitinspector --format=html --timeline --weeks --responsibilities >  ${SVNREPO}/stats.html
