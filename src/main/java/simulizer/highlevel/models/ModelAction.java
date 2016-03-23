2144b90e88b39ed18d89d980885e37ddb590c5e2
Kelsey McKenna <kelseyguitar55@googlemail.com>
2016-03-23 00:38:01 +0000

    Minor fix

--- src/main/java/simulizer/highlevel/models/ModelAction.java
+++ src/main/java/simulizer/highlevel/models/ModelAction.java
@@ -1,13 +1,13 @@
 package simulizer.highlevel.models;
 
 public class ModelAction<E> {
-	
+
 	public final E structure;
 	public final boolean skipable;
 
-	public ModelAction(E structure, boolean skipable){
+	public ModelAction(E structure, boolean skippable){
 		this.structure = structure;
-		this.skipable = skipable;
+		this.skipable = skippable;
 	}
-	
+
 }
