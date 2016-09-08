6713fdd8ecbf04c2b2ded3fadb07453ba9c540a6
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-09-08 19:11:38 +0100

    Improved CI test environment
    - No longer caching ace/spim
    - Using spim package instead of source
    - Using --info for gradle
    - Uploading test reports

--- install_spim.sh
+++ install_spim.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-if [ ! -e /usr/bin/spim ]; then
-  sudo apt-get install -y subversion bison g++ flex
-  svn checkout svn://svn.code.sf.net/p/spimsimulator/code/ spimsimulator
-  cd spimsimulator/spim
-  make
-  sudo make install
-fi
+sudo apt-get install -y subversion bison g++ flex
+svn checkout svn://svn.code.sf.net/p/spimsimulator/code/ spimsimulator
+cd spimsimulator/spim
+make
+sudo make install
